3acac093b1b65a200375119eb30d56fcc174905a
sunshine-2-me <givemesunshine110@gmail.com>
2024-11-05 17:31:45 -0500

    clear source code

--- nodes.py
+++ nodes.py
@@ -60,6 +60,7 @@ from scripts.reactor_swapper import (
     getFaceSwapModel, 
     insightface_path
 )
+from scripts.r_faceboost import swapper, restorer
 
 
 models_dir = folder_paths.models_dir
@@ -85,6 +86,7 @@ if "ultralytics" not in folder_paths.folder_names_and_paths:
     add_folder_path_and_extensions("ultralytics", [os.path.join(models_dir, "ultralytics")], folder_paths.supported_pt_extensions)
 if "sams" not in folder_paths.folder_names_and_paths:
     add_folder_path_and_extensions("sams", [os.path.join(models_dir, "sams")], folder_paths.supported_pt_extensions)
+
 
 
 def get_restorers():
@@ -122,74 +124,41 @@ def model_names():
     return {os.path.basename(x): x for x in models}
 
 
-DIR_REACTOR_V2_MODELS = os.path.join(models_dir, "reactor_v2")
-os.makedirs(DIR_REACTOR_V2_MODELS, exist_ok=True)
-
-FACE_DETECTION_MODELS_PATH = os.path.join(DIR_REACTOR_V2_MODELS, "face_detection_models")
-os.makedirs(FACE_DETECTION_MODELS_PATH, exist_ok=True)
-
-def get_face_detection_models():
-    models_path = os.path.join(FACE_DETECTION_MODELS_PATH, "*")
-    models = glob.glob(models_path)
-    models = [x for x in models if (x.endswith(".pth") or x.endswith(".onnx") or x.endswith(".pt"))]
-    if len(models) == 0:
-        fr_urls = [
-            "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov11n-face.pt",
-            "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov11s-face.pt",
-            "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov11m-face.pt",
-            # "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov10n-face.pt",
-            # "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov10s-face.pt",
-            # "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov10m-face.pt",
-            # "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov8n-face.pt",
-            # "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov8m-face.pt",
-            # "https://github.com/akanametov/yolo-face/releases/download/v0.0.0/yolov8l-face.pt"
-        ]
-        for model_url in fr_urls:
-            model_name = os.path.basename(model_url)
-            model_path = os.path.join(FACE_DETECTION_MODELS_PATH, model_name)
-            download(model_url, model_path, model_name)
-        models = glob.glob(models_path)
-        models = [x for x in models if (x.endswith(".pth") or x.endswith(".onnx") or x.endswith(".pt"))]
-    return models
-
-class FaceDetectionModelLoader:
+class FaceSwap:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "face_detection_model":(get_model_names(get_face_detection_models), ),
-            }
-        }
-    RETURN_TYPES = ("FACE_DETECTION_MODEL",)
-    FUNCTION = "load_face_detection_model"
-    CATEGORY = "🌌 ReActor_v2"
-    
-    def load_face_detection_model(self, face_detection_model):
-        self.face_detection_model = face_detection_model
-        self.face_detection_models_path = FACE_DETECTION_MODELS_PATH
-        if self.face_detection_model != "none":
-            face_model_path = os.path.join(self.face_detection_models_path, self.face_detection_model)
-            model = YOLO(face_model_path)
-        else:
-            model = None
-        return (model, )
-
-class FaceFeatureExtractor:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "compute_method": (["Mean", "Median", "Mode"], {"default": "Mean"}),
+                "input_image": ("IMAGE",),
+                "target_image": ("IMAGE",),
+                "swap_model": (list(model_names().keys()),),
+                "facedetection": (["retinaface_resnet50", "retinaface_mobile0.25", "YOLOv5l", "YOLOv5n"],),
+                "face_restore_model": (get_model_names(get_restorers),),
+                "face_restore_visibility": ("FLOAT", {"default": 1, "min": 0.1, "max": 1, "step": 0.05}),
+                "codeformer_weight": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1, "step": 0.05}),
+                "face_feature_update": ("BOOLEAN", {"default": True, "label_off": "OFF", "label_on": "ON"}),
+                "similarity_threshold":("FLOAT", {"default": 0.50, "min": 0.0, "max": 1.0, "step": 0.01}),
             },
             "optional": {
-                "images":("IMAGE", ),
-                "face_detection_model":("FACE_DETECTION_MODEL", ),
+                "source_image": ("IMAGE", ),
             }
         }
-    RETURN_TYPES = ("FACE_FEATURE", )
+    RETURN_TYPES = ("IMAGE", )
     FUNCTION = "execute"
     CATEGORY = "🌌 ReActor_v2"
     
+    def __init__(self):
+        self.similarity_threshold = 0.5
+        self.face_feature_update = True
+        self.face_features = []
+        self.face_boost_enabled = False
+        self.restore = True
+        self.boost_model = None
+        self.interpolation = "Bicubic"
+        self.boost_model_visibility = 1
+        self.boost_cf_weight = 0.5
+
+
     def extract_face_feature(self, image: Image.Image, det_size=(640, 640)):
         logging.StreamHandler.terminator = "\n"
         if image is None:
@@ -215,10 +184,9 @@ class FaceFeatureExtractor:
             # logger.error(no_face_msg)
             return no_face_msg
         
-
-    def execute(self, compute_method, images=None, face_detection_model=None):
-        if images is None or face_detection_model is None:
-            logger.error("Please provide `images` or `face_detection_model`")
+    def extract_feature(self, compute_method="Mean", images=None):
+        if images is None:
+            logger.error("Please provide `target_image`")
             return (None,)
         
         faces = []
@@ -234,47 +202,28 @@ class FaceFeatureExtractor:
             logger.status(f"Blending with Compute Method '{compute_method}'...")
             blended_embedding = np.mean(embeddings, axis=0) if compute_method == "Mean" else np.median(embeddings, axis=0) if compute_method == "Median" else stats.mode(embeddings, axis=0)[0].astype(np.float32)
             logger.status("--Done!--")
-        return (blended_embedding,)
-
-
-class FaceSwap:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "input_image": ("IMAGE",),
-                "swap_model": (list(model_names().keys()),),
-                "face_restore_model": (get_model_names(get_restorers),),
-                "face_feature_update": ("BOOLEAN", {"default": True, "label_off": "OFF", "label_on": "ON"}),
-                "similarity_threshold":("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0}),
-            },
-            "optional": {
-                "face_feature": ("FACE_FEATURE", ),
-                "source_image": ("IMAGE", ),
-                "face_detection_model":("FACE_DETECTION_MODEL", ),
-            }
-        }
-    RETURN_TYPES = ("IMAGE", )
-    FUNCTION = "execute"
-    CATEGORY = "🌌 ReActor_v2"
-    
-    def __init__(self):
-        self.similarity_threshold = 0.5
-        self.face_feature_update = True
-        self.face_features = []
+        return blended_embedding
+                
+    def execute(self, 
+                input_image, 
+                target_image, 
+                swap_model, 
+                facedetection, 
+                face_restore_model, 
+                face_restore_visibility, 
+                codeformer_weight, 
+                face_feature_update, 
+                similarity_threshold=0.5, 
+                source_image=None):
         
-    def execute(self, input_image, swap_model, face_restore_model, face_feature_update, similarity_threshold=0.5, face_feature=None, source_image=None, face_detection_model=None):
         self.similarity_threshold = similarity_threshold
         self.face_feature_update = face_feature_update
         
-        if face_feature is None:
-            logger.info("Please provide `face_feature`")
-            return (input_image,)
+        face_feature = self.extract_feature(images=target_image)
+        # print('face_feature=', face_feature)
+        
         if source_image is None:
             logger.info("Please provide `source_image`")
-            return (input_image,)
-        if face_detection_model is None:
-            logger.info("Please provide `face_detection_model`")
             return (input_image,)
             
         self.face_features.append(face_feature)
@@ -339,36 +288,192 @@ class FaceSwap:
                     target_face_single = target_faces[tindex]
                     if self.face_feature_update:
                         self.face_features.append(target_face_single.embedding)
-
+                
                 if target_face_single is not None:
-                    result = face_swapper.get(target_img, target_face_single, source_face)
-                    # bgr_fake, M = face_swapper.get(target_img, target_face_single, source_face, paste_back=False)
-                    # bgr_fake, scale = restorer.get_restored_face(bgr_fake, face_restore_model, face_restore_visibility, codeformer_weight, interpolation)
-                    # M *= scale
-                    # result = swapper.in_swap(target_img, bgr_fake, M)
+                    if self.face_boost_enabled:
+                        logger.status(f"Face Boost is enabled")
+                        result = face_swapper.get(target_img, target_face_single, source_face)
+                        bgr_fake, M = face_swapper.get(target_img, target_face_single, source_face, paste_back=False)
+                        bgr_fake, scale = restorer.get_restored_face(bgr_fake, face_restore_model, face_restore_visibility, codeformer_weight, interpolation)
+                        M *= scale
+                        result = swapper.in_swap(target_img, bgr_fake, M)
+                    else:
+                        result = face_swapper.get(target_img, target_face_single, source_face)
                 
                 results[i] = result
 
             result_images = [Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB)) for result in results]
             
             result_images = batched_pil_to_tensor(result_images)
+
+            if self.restore:
+                result_images = FaceSwap.restore_face(self,result_images,face_restore_model,face_restore_visibility,codeformer_weight,facedetection)
+            
         return (result_images, )
 
+
+    def restore_face(
+            self,
+            input_image,
+            face_restore_model,
+            face_restore_visibility,
+            codeformer_weight,
+            facedetection,
+        ):
+
+        result = input_image
+
+        if face_restore_model != "none" and not model_management.processing_interrupted():
+
+            global FACE_SIZE, FACE_HELPER
+
+            self.face_helper = FACE_HELPER
+            
+            faceSize = 512
+            if "1024" in face_restore_model.lower():
+                faceSize = 1024
+            elif "2048" in face_restore_model.lower():
+                faceSize = 2048
+
+            logger.status(f"Restoring with {face_restore_model} | Face Size is set to {faceSize}")
+
+            model_path = folder_paths.get_full_path("facerestore_models", face_restore_model)
+
+            device = model_management.get_torch_device()
+
+            if "codeformer" in face_restore_model.lower():
+
+                codeformer_net = ARCH_REGISTRY.get("CodeFormer")(
+                    dim_embd=512,
+                    codebook_size=1024,
+                    n_head=8,
+                    n_layers=9,
+                    connect_list=["32", "64", "128", "256"],
+                ).to(device)
+                checkpoint = torch.load(model_path)["params_ema"]
+                codeformer_net.load_state_dict(checkpoint)
+                facerestore_model = codeformer_net.eval()
+
+            elif ".onnx" in face_restore_model:
+
+                ort_session = set_ort_session(model_path, providers=providers)
+                ort_session_inputs = {}
+                facerestore_model = ort_session
+
+            else:
+
+                sd = comfy.utils.load_torch_file(model_path, safe_load=True)
+                facerestore_model = model_loading.load_state_dict(sd).eval()
+                facerestore_model.to(device)
+            
+            if faceSize != FACE_SIZE or self.face_helper is None:
+                self.face_helper = FaceRestoreHelper(1, face_size=faceSize, crop_ratio=(1, 1), det_model=facedetection, save_ext='png', use_parse=True, device=device)
+                FACE_SIZE = faceSize
+                FACE_HELPER = self.face_helper
+
+            image_np = 255. * result.numpy()
+
+            total_images = image_np.shape[0]
+
+            out_images = []
+
+            for i in range(total_images):
+
+                if total_images > 1:
+                    logger.status(f"Restoring {i+1}")
+
+                cur_image_np = image_np[i,:, :, ::-1]
+
+                original_resolution = cur_image_np.shape[0:2]
+
+                if facerestore_model is None or self.face_helper is None:
+                    return result
+
+                self.face_helper.clean_all()
+                self.face_helper.read_image(cur_image_np)
+                self.face_helper.get_face_landmarks_5(only_center_face=False, resize=640, eye_dist_threshold=5)
+                self.face_helper.align_warp_face()
+
+                restored_face = None
+
+                for idx, cropped_face in enumerate(self.face_helper.cropped_faces):
+
+                    # if ".pth" in face_restore_model:
+                    cropped_face_t = img2tensor(cropped_face / 255., bgr2rgb=True, float32=True)
+                    normalize(cropped_face_t, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
+                    cropped_face_t = cropped_face_t.unsqueeze(0).to(device)
+
+                    try:
+
+                        with torch.no_grad():
+
+                            if ".onnx" in face_restore_model: # ONNX models
+
+                                for ort_session_input in ort_session.get_inputs():
+                                    if ort_session_input.name == "input":
+                                        cropped_face_prep = prepare_cropped_face(cropped_face)
+                                        ort_session_inputs[ort_session_input.name] = cropped_face_prep
+                                    if ort_session_input.name == "weight":
+                                        weight = np.array([ 1 ], dtype = np.double)
+                                        ort_session_inputs[ort_session_input.name] = weight
+
+                                output = ort_session.run(None, ort_session_inputs)[0][0]
+                                restored_face = normalize_cropped_face(output)
+
+                            else: # PTH models
+
+                                output = facerestore_model(cropped_face_t, w=codeformer_weight)[0] if "codeformer" in face_restore_model.lower() else facerestore_model(cropped_face_t)[0]
+                                restored_face = tensor2img(output, rgb2bgr=True, min_max=(-1, 1))
+
+                        del output
+                        torch.cuda.empty_cache()
+
+                    except Exception as error:
+
+                        print(f"\tFailed inference: {error}", file=sys.stderr)
+                        restored_face = tensor2img(cropped_face_t, rgb2bgr=True, min_max=(-1, 1))
+
+                    if face_restore_visibility < 1:
+                        restored_face = cropped_face * (1 - face_restore_visibility) + restored_face * face_restore_visibility
+
+                    restored_face = restored_face.astype("uint8")
+                    self.face_helper.add_restored_face(restored_face)
+
+                self.face_helper.get_inverse_affine(None)
+
+                restored_img = self.face_helper.paste_faces_to_input_image()
+                restored_img = restored_img[:, :, ::-1]
+
+                if original_resolution != restored_img.shape[0:2]:
+                    restored_img = cv2.resize(restored_img, (0, 0), fx=original_resolution[1]/restored_img.shape[1], fy=original_resolution[0]/restored_img.shape[0], interpolation=cv2.INTER_AREA)
+
+                self.face_helper.clean_all()
+
+                # out_images[i] = restored_img
+                out_images.append(restored_img)
+
+                if state.interrupted or model_management.processing_interrupted():
+                    logger.status("Interrupted by User")
+                    return input_image
+
+            restored_img_np = np.array(out_images).astype(np.float32) / 255.0
+            restored_img_tensor = torch.from_numpy(restored_img_np)
+
+            result = restored_img_tensor
+
+        return result
+    
 
 NODE_CLASS_MAPPINGS = {
     # --- MAIN NODES ---
     "ReActorFaceSwap_v2": FaceSwap,
     # --- Additional Nodes ---
     
-    # Mine
-    "FaceDetectionModelLoader": FaceDetectionModelLoader,
-    "FaceFeatureExtractor": FaceFeatureExtractor,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     # --- MAIN NODES ---
     "ReActorFaceSwap_v2": "ReActor_v2 🌌 Face Swap(Embedding)",
-    # --- Operations with Face Models ---
-    "FaceDetectionModelLoader": "ReActor_v2 🌌 FaceDetectionModelLoader",
-    "FaceFeatureExtractor": "ReActor_v2 🌌 FaceFeatureExtractor",
+    # --- Additional Nodes ---
+
 }
